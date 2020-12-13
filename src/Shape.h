12c8f2c125e96100c84c076e5d4cdd965bc9e236
Timothy Blut <batzer94@gmail.com>
2020-12-13 20:07:14 +0100

    Add description to shape sampling and pdf methods

--- src/Shape.h
+++ src/Shape.h
@@ -13,7 +13,11 @@ public:
     virtual ~Shape() = default;
     virtual float intersect(const Ray& ray) const = 0;
     virtual Vec3 normalAt(const Vec3& p) const = 0;
+
+    // Returns a uniformly sampled direction from the point p to this shape
     virtual Vec3 sampleDirection(const Vec3& p, float u1, float u2, float* pdf = nullptr) const = 0;
+
+    // Returns the PDF of uniformly sampled directions from the point p 
     virtual float pdf(const Vec3& p) const = 0;
 
     bool isLight() const {
