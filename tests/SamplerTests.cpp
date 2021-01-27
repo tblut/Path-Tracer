42cef6a438b890a0f68f04c0da5132a773df42db
Timothy Blut <batzer94@gmail.com>
2021-01-27 01:17:52 +0100

    Fix test for correct n-rooks condition

--- tests/SamplerTests.cpp
+++ tests/SamplerTests.cpp
@@ -47,7 +47,8 @@ TEST_CASE("CMJ Sampling") {
 
     std::vector<uint32_t> strata1D(N, 0);
     std::vector<uint32_t> strata2D(N, 0);
-    std::vector<uint32_t> substrata2D(N * N, 0);
+    std::vector<uint32_t> substrataRows2D(N, 0);
+    std::vector<uint32_t> substrataCols2D(N, 0);
 
     auto strataIndex = [](const pt::Vec2& sample, uint32_t m, uint32_t n) {
         uint32_t strataX = static_cast<uint32_t>(sample.x * m);
@@ -56,37 +57,38 @@ TEST_CASE("CMJ Sampling") {
     };
 
     pt::CMJSampler sampler(N);
-    for (uint32_t pixelIndex = 0; pixelIndex < 100; pixelIndex++) {
+    for (uint32_t pixelIndex = 0; pixelIndex < 10; pixelIndex++) {
         sampler.startPixel(pixelIndex);
 
         std::fill(strata1D.begin(), strata1D.end(), 0);
         std::fill(strata2D.begin(), strata2D.end(), 0);
-        std::fill(substrata2D.begin(), substrata2D.end(), 0);
+        std::fill(substrataRows2D.begin(), substrataRows2D.end(), 0);
+        std::fill(substrataCols2D.begin(), substrataCols2D.end(), 0);
 
         for (uint32_t sampleIndex = 0; sampleIndex < N; sampleIndex++) {
             float sample1D = sampler.get1D();
             pt::Vec2 sample2D = sampler.get2D();
 
             uint32_t index = static_cast<uint32_t>(sample1D * N);
-            CHECK(strata1D[index] == 0);
             strata1D[index]++;
 
-            index = strataIndex(sample2D, m, n);
-            CHECK(strata2D[index] == 0);
+            uint32_t strataX = static_cast<uint32_t>(sample2D.x * m);
+            uint32_t strataY = static_cast<uint32_t>(sample2D.y * n);
+            index = strataX + strataY * m;
             strata2D[index]++;
 
-            index = strataIndex(sample2D, N, N);
-            CHECK(substrata2D[index] == 0);
-            substrata2D[index]++;
+            uint32_t substrataRow = static_cast<uint32_t>(sample2D.y * N);
+            substrataRows2D[substrataRow]++;
+
+            uint32_t substrataCol = static_cast<uint32_t>(sample2D.x * N);
+            substrataCols2D[substrataCol]++;
 
             sampler.startNextSample();
         }
 
-        for (uint32_t count : strata1D) {
-            CHECK(count == 1);
-        }
-        for (uint32_t count : strata2D) {
-            CHECK(count == 1);
-        }
+        for (uint32_t count : strata1D) CHECK(count == 1);
+        for (uint32_t count : strata2D) CHECK(count == 1);
+        for (uint32_t count : substrataRows2D) CHECK(count == 1);
+        for (uint32_t count : substrataCols2D) CHECK(count == 1);
     }
 }
